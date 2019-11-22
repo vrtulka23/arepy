8e371349dc4e01f194db4559a80ee684fe5e13b2
vrtulka23 <ondrej.jaura@uni-heidelberg.de>
2019-11-22 14:19:31 +0100

    Added example projects

---
+++ python/scripy/examples/plots/grids/sphere.py
@@ -0,0 +1,17 @@
+import arepy as apy
+import numpy as np
+from scripy.examples.plots.grids import grids
+
+class sphere(grids):
+
+    def init(self):
+        self.setFigure(1,1,1,srow=4,scol=4,show=True,fileFormat='png')
+
+    def plot(self):
+
+        lim = [-1000,1000]
+        sp = self.fig.getSubplot(0,0,title='Grid of rays',xlabel='x',ylabel='y',zlabel='z',
+                                 xyz=True, xlim=lim, ylim=lim, zlim=lim)
+        grid = apy.coord.gridSphere([30],extent=[0.1,1000])
+        x,y,z = grid.coords.T
+        sp.addScatter(x,y,z,s=1,marker='.')
